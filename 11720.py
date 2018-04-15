173580d66f6233333fc859cab6b35296e3f5297f
곽창수 <cieske@daum.net>
2018-04-15 23:44:33 +0900

    exercises codes

---
+++ 11720.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 15 23:37:01 2018
+
+@author: ciesk
+"""
+
+a = int(input())
+b = list(input())
+c = 0
+
+for i in b:
+    c += int(i)
+    
+print(c)
